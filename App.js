1aaab3e0ee436b977aa07ba7a90b2f0e2d337e25
Rob Scott <rdavidscott1@gmail.com>
2020-04-01 17:43:09 -0700

    reorganized imports/filepaths for screen directory

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
-import FailedVoteCounter from "./components/FailedVoteCounter";
-import ImageOrTimer from "./components/ImageOrTimer";
-import PlayerList from "./components/PlayersList";
-import Scoreboard from "./components/Scoreboard";
-import ButtonMenu from "./components/ButtonMenu";
+import GameScreen from "./screens/Game";
+import StartScreen from "./screens/Start";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FailedVoteCounter />
-      <ImageOrTimer />
-      <PlayerList />
-      <Scoreboard />
-      <ButtonMenu />
+      <StartScreen />
     </View>
   );
 }
