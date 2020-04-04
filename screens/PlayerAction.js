303693900adaf08f0495b49376594736d9399cfe
Rob Scott <rdavidscott1@gmail.com>
2020-04-04 16:01:05 -0700

    Transformed to class-structured component for my own sake

--- screens/PlayerAction.js
+++ screens/PlayerAction.js
@@ -4,29 +4,72 @@ import FailedVoteCounter from "../components/FailedVoteCounter";
 import ImageOrTimer from "../components/ImageOrTimer";
 import ElectBtn from "../components/ElectBtn";
 import KillBtn from "../components/KillBtn";
+import { render } from "react-dom";
 
-const PlayerActionScreen = props => {
-  return (
-    <View style={styles.container}>
-      <FailedVoteCounter />
-      <ImageOrTimer />
-      <ElectBtn name={props.name} style={styles.btn} />
-      <KillBtn name={props.name} style={styles.btn} />
-    </View>
-  );
-};
+class PlayerActionScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.killPlayer = this.killPlayer.bind(this);
+    this.electChancellor = this.electChancellor.bind(this);
+  }
+
+  state = {};
+
+  killPlayer(player) {
+    this.props.killPlayer(player);
+    this.props.navigation.navigate("game");
+  }
+
+  electChancellor(player) {
+    this.props.electChancellor(player);
+    this.props.navigation.navigate("game");
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FailedVoteCounter />
+        <ImageOrTimer />
+        <ElectBtn
+          name={props.player}
+          style={styles.btn}
+          onPress={this.electChancellor(props.player)}
+        />
+        <KillBtn
+          name={props.player}
+          style={styles.btn}
+          onPress={this.killPlayer(props.player)}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#434343",
     height: "100%",
     width: "100%",
-    padding: 5
+    padding: 5,
   },
   btn: {
     paddingHorizontal: 20,
-    marginVertical: 45
-  }
+    marginVertical: 45,
+  },
 });
 
-export default PlayerActionScreen;
+const mapStateToProps = (state) => ({
+  ...state.playerReducer,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  killPlayer: (player) => {
+    dispatch(killPlayer(player));
+  },
+  electChancellor: (player) => {
+    dispatch(electChancellor(player));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlayerActionScreen);
