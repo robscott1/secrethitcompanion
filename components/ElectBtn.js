22c4cffea0bf5a1eae1d9657ba81ea7c556822d6
Rob Scott <rdavidscott1@gmail.com>
2020-04-04 21:07:13 -0700

    Change syntax to plassing entire object
    passes entire object so that changes can be made once a button is pressed-- changed dot notation to access id from props.    (props.player.id)

--- components/ElectBtn.js
+++ components/ElectBtn.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const ElectBtn = props => {
+const ElectBtn = (props) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.btn}>
-        <Text>Elect {props.name} as Chancellor</Text>
+        <Text>Elect {props.player.id} as Chancellor</Text>
       </TouchableOpacity>
     </View>
   );
@@ -15,12 +15,12 @@ const styles = StyleSheet.create({
   btn: {
     backgroundColor: "#F7E2C1",
     borderRadius: 5,
-    height: "100%"
+    height: "100%",
   },
   container: {
     height: 55,
-    width: "80%"
-  }
+    width: "80%",
+  },
 });
 
 export default ElectBtn;
--- components/KillBtn.js
+++ components/KillBtn.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const KillBtn = props => {
+const KillBtn = (props) => {
   return (
     <View>
       <TouchableOpacity style={styles.btn}>
-        <Text>Kill {props.name} </Text>
+        <Text>Kill {props.player.id} </Text>
       </TouchableOpacity>
     </View>
   );
@@ -15,12 +15,12 @@ const styles = StyleSheet.create({
   btn: {
     backgroundColor: "#F7E2C1",
     borderRadius: 5,
-    height: "100%"
+    height: "100%",
   },
   container: {
     height: 55,
-    width: "80%"
-  }
+    width: "80%",
+  },
 });
 
 export default KillBtn;
